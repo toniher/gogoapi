bd5afc2b610e4b27249e92a42cc1249a5779b920
toniher <toniher@cau.cat>
2015-12-23 18:24:17 +0100

    moving case no connection

--- functions/mysql.js
+++ functions/mysql.js
@@ -77,84 +77,88 @@ exports.getGO = function( item, listGO, listdesc, res, callback ) {
 
 	pool.getConnection(function(err, connection) {
 
-		// First we check whether this exists in goassociation, if so, we are done.
-		var sql = 'SELECT distinct(a.GO), t.term_type, t.name from goassociation a, term t where t.acc=a.GO AND a.`UniProtKB-AC` = ' + connection.escape(item);
-		var sql2 = 'SELECT distinct(a.GO), t.term_type, t.name from goassociation a, term t, idmapping i where t.acc=a.GO AND a.`UniProtKB-AC` = i.`UniProtKB-AC` and i.ID=' + connection.escape(item);
+		if ( ! err ) {
 
-		connection.query(sql, function(err, results) {
+			// First we check whether this exists in goassociation, if so, we are done.
+			var sql = 'SELECT distinct(a.GO), t.term_type, t.name from goassociation a, term t where t.acc=a.GO AND a.`UniProtKB-AC` = ' + connection.escape(item);
+			var sql2 = 'SELECT distinct(a.GO), t.term_type, t.name from goassociation a, term t, idmapping i where t.acc=a.GO AND a.`UniProtKB-AC` = i.`UniProtKB-AC` and i.ID=' + connection.escape(item);
+	
+			connection.query(sql, function(err, results) {
 
-			if ( err ) {
-				functions.sendError( connection, res, err );
-				connection.release();
-			} else {
-	
-				var golist = [];
-		
-				if ( results.length === 0 ) {
-	
-					connection.query(sql2, function(err, results) {
-	
-						if ( err ) {
-							functions.sendError( connection, res, err );
-							connection.release();
-						} else {
-			
-							var golist = [];
-	
-							if ( results.length > 0 ) {
-	
-								async.each( results, function( result, callback2 ){
-									
-									golist.push( { acc: result.GO, term_type: result.term_type, name: result.name } );
-									callback2();
-								},
-								function( err ) {
-	
-									for ( var i = 0; i < golist.length; i++ ) {
-	
-										var term_type = golist[i].term_type;
-										var acc = golist[i].acc;
-										var name = golist[i].name;
-										listGO[term_type].push( acc );
-										listdesc[ acc ] = name;
-										//listGO[term_type].pushIfNotExist( acc, function(e) { 
-										//	return e === acc; 
-										//});
-									}
-	
-									callback();
-									connection.release();
-								});
-			
-							} else {
-								// Temporary sendError
-								functions.sendError( connection, res, err );
-								connection.release();
-							}
-						}
-		
-					});
-		
-		
+				if ( err ) {
+					functions.sendError( connection, res, err );
+					connection.release();
 				} else {
 		
-					for ( var i = 0; i < results.length; i++ ) {
+					var golist = [];
+			
+					if ( results.length === 0 ) {
 		
-						var term_type = results[i].term_type;
-						var acc = results[i].GO;
-						var name = results[i].name;
-						listGO[term_type].push( acc );
-						listdesc[ acc ] = name;
-						//listGO[term_type].pushIfNotExist( acc,  function(e) { 
-						//	return e === acc; 
-						//});
+						connection.query(sql2, function(err, results) {
+		
+							if ( err ) {
+								functions.sendError( connection, res, err );
+								connection.release();
+							} else {
+				
+								var golist = [];
+		
+								if ( results.length > 0 ) {
+		
+									async.each( results, function( result, callback2 ){
+										
+										golist.push( { acc: result.GO, term_type: result.term_type, name: result.name } );
+										callback2();
+									},
+									function( err ) {
+		
+										for ( var i = 0; i < golist.length; i++ ) {
+		
+											var term_type = golist[i].term_type;
+											var acc = golist[i].acc;
+											var name = golist[i].name;
+											listGO[term_type].push( acc );
+											listdesc[ acc ] = name;
+											//listGO[term_type].pushIfNotExist( acc, function(e) { 
+											//	return e === acc; 
+											//});
+										}
+		
+										callback();
+										connection.release();
+									});
+				
+								} else {
+									// Temporary sendError
+									functions.sendError( connection, res, err );
+									connection.release();
+								}
+							}
+			
+						});
+			
+			
+					} else {
+			
+						for ( var i = 0; i < results.length; i++ ) {
+			
+							var term_type = results[i].term_type;
+							var acc = results[i].GO;
+							var name = results[i].name;
+							listGO[term_type].push( acc );
+							listdesc[ acc ] = name;
+							//listGO[term_type].pushIfNotExist( acc,  function(e) { 
+							//	return e === acc; 
+							//});
+						}
+						callback();
+						connection.release();
 					}
-					callback();
-					connection.release();
 				}
-			}
-	
-		});
+		
+			});
+
+		}
 
 	});
 
